64acf664f6c7f8096cb422132eee5256d0516ff7
AbdallahMostafa <abdallahmostafa377@gmail.com>
2021-09-04 20:34:17 +0200

    added a list and a function to get an item by id

--- app/controllers/TodoListController.scala
+++ app/controllers/TodoListController.scala
@@ -3,12 +3,33 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import play.api.libs.json._
+import collection.mutable
+import models.TodoListItem
 
 @Singleton
 class TodoListController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
+    private val todoList = new mutable.ListBuffer[TodoListItem]()
+    todoList += TodoListItem(1, "test", true)
+    todoList += TodoListItem(2, "some other value", false)
+    
+    implicit val todoListJson = Json.format[TodoListItem]
 
     def getAll(): Action[AnyContent] = Action {
-        NoContent
+        if (todoList.isEmpty) {
+            NoContent
+        } else {
+            Ok(Json.toJson(todoList))
+        }
     }
+
+    def getById(itemId: Long) = Action {
+        val foundItem = todoList.find(_.id == itemId)
+        foundItem match {
+            case Some(item) => Ok(Json.toJson(item))
+            case None => NotFound
+        }
+    }
+
 }
